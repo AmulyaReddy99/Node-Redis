5692459dd76e405a3948103a82012f91bbedcda5
AmulyaReddy99 <amulyareddyk@yahoo.in>
2021-01-20 18:37:38 +0530

    redis chache response

--- node_redis_cache.js
+++ node_redis_cache.js
@@ -6,31 +6,23 @@ const PORT = process.env.PORT || 5000;
 const REDIS_PORT = process.env.PORT || 6379;
 
 const client = redis.createClient(REDIS_PORT);
-
 const app = express();
 
 // Set response
-function setResponse(username, repos) {
-  return `<h2>${username} has ${repos} Github repos</h2>`;
+function setResponse(data) {
+  return JSON.parse(JSON.stringify(data));
 }
 
 // Make request to Github for data
 async function getRepos(req, res, next) {
   try {
     console.log('Fetching Data...');
-
-    const { username } = req.params;
-
-    const response = await fetch(`https://api.github.com/users/${username}`);
-
+    const response = await fetch(`https://opentdb.com/api.php?amount=49`);
     const data = await response.json();
-
-    const repos = data.public_repos;
-
     // Set data to Redis
-    client.setex(username, 3600, repos);
+    client.setex("questions", 3600, JSON.stringify(data));
 
-    res.send(setResponse(username, repos));
+    res.send(setResponse(data));
   } catch (err) {
     console.error(err);
     res.status(500);
@@ -39,20 +31,18 @@ async function getRepos(req, res, next) {
 
 // Cache middleware
 function cache(req, res, next) {
-  const { username } = req.params;
-
-  client.get(username, (err, data) => {
+  client.get("questions", (err, data) => {
     if (err) throw err;
 
     if (data !== null) {
-      res.send(setResponse(username, data));
+      res.send(setResponse(data));
     } else {
       next();
     }
   });
 }
 
-app.get('/repos/:username', cache, getRepos);
+app.get('/getJson', cache, getRepos);
 
 app.listen(5000, () => {
   console.log(`App listening on port ${PORT}`);
